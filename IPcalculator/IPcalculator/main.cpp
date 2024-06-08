876c3bb553f07e981c77010481caefafb326be30
Clayman <Clayman@DESKTOP-DM8E77U>
2024-06-08 15:20:56 +0300

    Prefix set mask DONE

--- IPcalculator/IPcalculator/main.cpp
+++ IPcalculator/IPcalculator/main.cpp
@@ -20,35 +20,79 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HWND hSpin = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
 		SendMessage(hSpin, UDM_SETRANGE32, 0, 32);
+
+		SetFocus(GetDlgItem(hwnd, IDC_IPADDRESS));
 	}
 	break;
 	case WM_COMMAND:
 	{
+		CONST INT SIZE = 5;
+		CHAR sz_prefix[SIZE]{};
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
-		HWND hEditPrefox = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
 		{
+			//if (HIWORD(wParam) == EN_CHANGE)
+			//{
+			//	//BYTE = 8 bit
+			//	//WORD = 2 BYTE = 16 bit;
+			//	//DWORD = 4 BYTE = 32 bit;
+			//	//QWORD = 8 BYTE = 64 bit;
+			//	//TBYTE = 10 BYTE = 80 bit;
+			//	//DQWORD = 16 BYTE = 128 bit;
+			//	DWORD dwAddress = 0;
+			//	//https://learn.microsoft.com/en-us/windows/win32/controls/ipm-getaddress
+			//	SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwAddress);
+			//	BYTE first = FIRST_IPADDRESS(dwAddress);
+			//	BYTE dw_prefix = 0;
+			//	if (first < 128)dw_prefix = 8;
+			//	else if (first < 192)dw_prefix = 16;
+			//	else if (first < 224)dw_prefix = 24;
+			//	CHAR sz_prefix[5]{};
+			//	sprintf(sz_prefix, "%i", dw_prefix);
+			//	SendMessage(hEditPrefox, WM_SETTEXT, 0, (LPARAM)sz_prefix);
+			//}
+		}
+		break;
+		case IDC_EDIT_PREFIX:
+		{
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
-				//BYTE = 8 bit
-				//WORD = 2 BYTE = 16 bit;
-				//DWORD = 4 BYTE = 32 bit;
-				//QWORD = 8 BYTE = 64 bit;
-				//TBYTE = 10 BYTE = 80 bit;
-				//DQWORD = 16 BYTE = 128 bit;
-				DWORD dwAddress = 0;
-				//https://learn.microsoft.com/en-us/windows/win32/controls/ipm-getaddress
-				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwAddress);
-				BYTE first = FIRST_IPADDRESS(dwAddress);
-				BYTE dw_prefix = 0;
-				if (first < 128)dw_prefix = 8;
-				else if (first < 192)dw_prefix = 16;
-				else if (first < 224)dw_prefix = 24;
+				//MessageBox(hwnd, "Profix changed", "Notification", MB_OK);
+				SendMessage(hEditPrefix, WM_GETTEXT, SIZE, (LPARAM)sz_prefix);
+				DWORD dw_prefix = atoi(sz_prefix);
+				DWORD dw_mask = UINT_MAX;
+				ULONG_MAX;
+				//dw_mask >>= (32 - dw_prefix);
+				dw_mask <<= (32 - dw_prefix);
+				SendMessage(hIPmask, IPM_SETADDRESS, 0, dw_mask);
+			}
+		}
+		break;
+		}
+	}
+	break;
+	case WM_NOTIFY:
+	{
+		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		switch (wParam)
+		{
+		case IDC_IPADDRESS:
+		{
+			NMIPADDRESS* pAddress = ((NMIPADDRESS*)(lParam));
+			if (pAddress->iField == 0)
+			{
+				DWORD dw_prefix = 0;
+				if (pAddress->iValue < 128)dw_prefix = 8;
+				else if (pAddress->iValue < 192)dw_prefix = 16;
+				else if (pAddress->iValue < 224)dw_prefix = 24;
 				CHAR sz_prefix[5]{};
 				sprintf(sz_prefix, "%i", dw_prefix);
-				SendMessage(hEditPrefox, WM_SETTEXT, 0, (LPARAM)sz_prefix);
+				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)sz_prefix);
 			}
 		}
 		break;
